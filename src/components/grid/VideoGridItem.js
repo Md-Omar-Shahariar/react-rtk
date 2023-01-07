076114f174c025b42e7624c52412959ece3bb55e
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-07 21:53:27 +0600

    route done

--- src/components/grid/VideoGridItem.js
+++ src/components/grid/VideoGridItem.js
@@ -6,7 +6,7 @@ const VideoGridItem = () => {
     <div className="col-span-12 sm:col-span-6 md:col-span-3 duration-300 hover:scale-[1.03]">
       <div className="w-full flex flex-col">
         <div className="relative">
-          <Link to="video/1">
+          <Link to="/video/1">
             <img
               src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
               className="w-full h-auto"
@@ -20,7 +20,7 @@ const VideoGridItem = () => {
         </div>
 
         <div className="flex flex-row mt-2 gap-2">
-          <Link to="video/1" className="shrink-0">
+          <Link to="/video/1" className="shrink-0">
             <img
               src="https://avatars.githubusercontent.com/u/73503432?v=4"
               className="rounded-full h-6 w-6"
@@ -28,18 +28,18 @@ const VideoGridItem = () => {
             />
           </Link>
 
-          <div clas="flex flex-col">
-            <a href="video.html">
+          <div class="flex flex-col">
+            <Link to="video/1">
               <p className="text-slate-900 text-sm font-semibold">
                 Video title
               </p>
-            </a>
-            <a
+            </Link>
+            <Link
               className="text-gray-400 text-xs mt-2 hover:text-gray-600"
-              href="#"
+              to="/video/1"
             >
               Learn with Sumit
-            </a>
+            </Link>
             <p className="text-gray-400 text-xs mt-1">
               200 views . May 3, 2022
             </p>
