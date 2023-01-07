6a0b91330ce556c8e49be161a0a019a14bebb920
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-07 14:48:46 +0600

    ui

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Tags from "../components/tag/Tags";
+import VideoGrid from "../components/grid/VideoGrid";
+import Pagination from "../components/ui/Pagination";
+import Footer from "../components/footer/Footer";
+import Navbar from "../components/navbar/Navbar";
+
+const Home = () => {
+  return (
+    <>
+      <Navbar></Navbar>
+      <Tags></Tags>
+      <VideoGrid></VideoGrid>
+      <Pagination></Pagination>
+      <Footer></Footer>
+    </>
+  );
+};
+
+export default Home;
