9cb8843a0b5eeef465154d75bfceb6eaf738e637
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-09 01:26:16 +0600

    video fetch

--- src/pages/Video.js
+++ src/pages/Video.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 import Player from "../components/description/Player";
 import VideoDescription from "../components/description/VideoDescription";
 
 import VideoList from "../components/description/VideoList";
+import { useParams } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+import { fetchVideo } from "../features/video/videoSlice";
 
 const Video = () => {
+  const { videoId } = useParams();
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchVideo(videoId));
+  }, []);
+
   return (
     <section className="pt-6 pb-20">
       <div className="mx-auto max-w-7xl px-2 pb-20 min-h-[400px]">
