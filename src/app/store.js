816ebfcac4e8c69fd83cdba7a41bf7f121d484c9
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-10 15:45:03 +0600

    store

--- src/app/store.js
+++ src/app/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import videosReducer from "../features/videos/videosSlice";
 import tagReducer from "../features/tags/tagSlice";
 import videoReducer from "../features/video/videoSlice";
+import relatedVideoReducer from "../features/relatedVideos/relatedVideoSlice";
 
 export const store = configureStore({
   reducer: {
     videos: videosReducer,
     tags: tagReducer,
     video: videoReducer,
+    relatedVideo: relatedVideoReducer,
   },
 });
--- src/components/description/VideoList.js
+++ src/components/description/VideoList.js
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import SingleVideo from "./SingleVideo";
+import { useDispatch } from "react-redux";
+import { fetchRelatedVideo } from "../../features/relatedVideos/relatedVideoSlice";
 
 const VideoList = ({ currentVideoId, tags }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchRelatedVideo({ tags, id: currentVideoId }));
+  }, [dispatch, tags, currentVideoId]);
   return (
     <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
       {/* <!-- single related video --> */}
