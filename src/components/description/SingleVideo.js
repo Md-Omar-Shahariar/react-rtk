2f8f89ab1279ae5994944d6834858283a599a6c1
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-10 15:55:06 +0600

    related video

--- src/components/description/SingleVideo.js
+++ src/components/description/SingleVideo.js
@@ -1,19 +1,22 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-const SingleVideo = () => {
+const SingleVideo = ({ video = {} }) => {
+  const { id, thumbnail, title, duration, author, avatar, link, views, date } =
+    video;
+
   return (
     <div className="w-full flex flex-row gap-2 mb-4">
       <div className="relative w-[168px] h-[94px] flex-none duration-300 hover:scale-[1.03]">
         <Link to="/video/1">
           <img
-            src="https://i3.ytimg.com/vi/6O4s7v28nlw/maxresdefault.jpg"
+            src={thumbnail}
             className="object-cover"
             alt="Some video title"
           />
         </Link>
         <p className="absolute right-2 bottom-2 bg-gray-900 text-gray-100 text-xs px-1 py">
-          12:10
+          {duration}
         </p>
       </div>
 
--- src/components/description/VideoList.js
+++ src/components/description/VideoList.js
@@ -1,18 +1,35 @@
 import React, { useEffect } from "react";
 import SingleVideo from "./SingleVideo";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchRelatedVideo } from "../../features/relatedVideos/relatedVideoSlice";
+import Loading from "../ui/Loading";
 
 const VideoList = ({ currentVideoId, tags }) => {
   const dispatch = useDispatch();
+  const { relatedVideo, isLoading, isError, error } = useSelector(
+    (state) => state.relatedVideo
+  );
 
   useEffect(() => {
     dispatch(fetchRelatedVideo({ tags, id: currentVideoId }));
   }, [dispatch, tags, currentVideoId]);
+  let content = null;
+
+  if (isLoading) content = <Loading></Loading>;
+  if (!isLoading && isError)
+    content = <div className="col-span-12">{error}</div>;
+  if (!isLoading && !isError && relatedVideo.length > 0) {
+    content = relatedVideo.map((video) => (
+      <SingleVideo key={video.id} video={video}></SingleVideo>
+    ));
+  }
+  if (!isLoading && !isError && relatedVideo.length === 0) {
+    content = <div className="col-span-12">No Related Videos</div>;
+  }
   return (
     <div className="col-span-full lg:col-auto max-h-[570px] overflow-y-auto">
       {/* <!-- single related video --> */}
-      <SingleVideo></SingleVideo>
+      {content}
     </div>
   );
 };
