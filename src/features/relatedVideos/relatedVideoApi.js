c37a1b0c25c36016e6aad5b71a5ff35727397045
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-10 16:04:59 +0600

    related video done

--- src/features/relatedVideos/relatedVideoApi.js
+++ src/features/relatedVideos/relatedVideoApi.js
@@ -4,9 +4,10 @@ export const getRelatedVideo = async ({ tags, id }) => {
   const limit = 5;
   let query =
     tags?.length > 0
-      ? tags.map((tag) => `tags_like${tag}`).join("&") +
+      ? tags.map((tag) => `tags_like=${tag}`).join("&") +
         `&id_ne=${id}&_limit=${limit}`
       : `&id_ne=${id}&_limit=${limit}`;
+  console.log(query);
   const response = await axios.get(`/videos?${query}`);
   return response.data;
 };
