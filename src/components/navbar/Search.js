899476924be1e9f40b18b60a6b49ecfd4fdfd431
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-14 00:22:42 +0600

    navigate

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,14 +1,20 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { search } from "../../features/filter/filterSlice";
+import { useMatch, useNavigate } from "react-router-dom";
 
 const Search = () => {
   const dispatch = useDispatch();
   const { searchText } = useSelector((state) => state.filter);
   const [input, setInput] = useState(searchText);
+  const match = useMatch("/");
+  const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
     dispatch(search(input));
+    if (!match) {
+      navigate("/");
+    }
   };
   return (
     <form onSubmit={handleSubmit}>
