1e0db38008c13892775947ce82c5e196ea8d7b00
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-13 15:55:06 +0600

    filter feature

--- src/components/navbar/Search.js
+++ src/components/navbar/Search.js
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { search } from "../../features/filter/filterSlice";
 
 const Search = () => {
+  const dispatch = useDispatch();
+  const { searchText } = useSelector((state) => state.filter);
+  const [input, setInput] = useState(searchText);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(search(input));
+  };
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <input
+        value={input}
         className="outline-none border-none mr-2"
         type="search"
         name="search"
         placeholder="Search"
+        onChange={(e) => setInput(e.target.value)}
       />
     </form>
   );
