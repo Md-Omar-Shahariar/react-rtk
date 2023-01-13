0082d9e309761f2a60e822c29138b4611bc80107
Md-Omar-Shahariar <osafridi00@gmail.com>
2023-01-13 15:29:17 +0600

    filter reducer

--- src/app/store.js
+++ src/app/store.js
@@ -4,6 +4,7 @@ import videosReducer from "../features/videos/videosSlice";
 import tagReducer from "../features/tags/tagSlice";
 import videoReducer from "../features/video/videoSlice";
 import relatedVideoReducer from "../features/relatedVideos/relatedVideoSlice";
+import filterReducer from "../features/filter/filterSlice";
 
 export const store = configureStore({
   reducer: {
@@ -11,5 +12,6 @@ export const store = configureStore({
     tags: tagReducer,
     video: videoReducer,
     relatedVideo: relatedVideoReducer,
+    filter: filterReducer,
   },
 });
